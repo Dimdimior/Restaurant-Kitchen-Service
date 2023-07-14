658dd3d877dfc20b49d9c3f193845c11f4b0d0bb
Dmitry <dimior1992@gmail.com>
2023-07-14 13:40:38 +0300

    Added base and sidebar templates

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
+from restaurant.models import Dish, DishType, Cook
+
+
 # Create your views here.
+
+
+def index(request):
+    num_dishes = Dish.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_cooks = Cook.objects.count()
+
+    context = {
+        "num_dishes": num_dishes,
+        "num_dish_types": num_dish_types,
+        "num_cooks": num_cooks,
+    }
+    return render(request, "restaurant/index.html", context=context)
